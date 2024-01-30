9a1734e69e3b00abfca7ce42c9e4fd75e954807a
swflora <154944599+swflora@users.noreply.github.com>
2024-01-30 15:38:02 +0900

    first.py

--- first.py
+++ first.py
@@ -11,47 +11,19 @@ import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
 
+@st.cache
+def load_data():
+    df = pd.read_csv("aisles.csv")
+    return df
+
 def main():
-    st.markdown("<h1 style='text-align: center; color: blue;'>Instacart Market</h1>", unsafe_allow_html=True)
-
-    with st.sidebar:
-        st.header("Sidebar")
-        day = st.selectbox("Select a day", ["Thur", "Fri", "Sat", "Sun"])
-
-    tips = sns.load_dataset("tips")
-    filtered_tips = tips[tips["day"] == day]
-    top_bill = filtered_tips["total_bill"].max()
-    top_tip = filtered_tips["tip"].max()
-
-    tab1, tab2, tab3 = st.tabs(['Total Bill', 'Tip', "Size"])
-    with tab1: #total bill
-        fig, ax = plt.subplots()
-        st.header("Total Bill Amounts")
-        sns.histplot(filtered_tips["total_bill"], kde=False, ax=ax)
-        st.pyplot(fig)
-
-    with tab2:
-        fig, ax = plt.subplots()
-        st.header("Tip Amounts")
-        sns.histplot(filtered_tips["tip"], kde=False, ax=ax)
-        st.pyplot(fig)
-
-    with tab3:
-        fig, ax = plt.subplots()
-        st.header("Table Sizes")
-        sns.boxplot(data=filtered_tips, x="sex", y="tip", ax=ax)
-        st.pyplot(fig)
-
-    col1, col2, col3 = st.columns([1, 1, 1])
-    with col1:
-        st.metric("Top Bill", f"${top_bill:.2f}")
-
-    with col2:
-        st.metric("Top Tip", f"${top_tip:.2f}")
-
-    with col3:
-        st.write("col3")
-
+    st.markdown("<h1 style='text-align: center; color: #623AA2;'>Instacart Market</h1>", unsafe_allow_html=True)
+    
+    st.title("Hello World on Streamlit.io")
+    
+    aisles = load_data()
+    
+    st.table(aisles)
 
 if __name__ == "__main__":
     main()
