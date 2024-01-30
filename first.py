eb32693b529d924187bcbe312e74d7ead3ef6c5a
swflora <154944599+swflora@users.noreply.github.com>
2024-01-30 16:10:35 +0900

    first.py

--- first.py
+++ first.py
@@ -22,8 +22,8 @@ def main():
     st.title("aisles")
     aisles = load_date()
     
-    value1 = st.slider("Choose the aisle'id", 1, 134)
-    st.sidebar.write(value1)
+    # value1 = st.slider("Choose the aisle'id", 1, 134)
+    
     #select_aisle = aisles[aisles['aisle_id'] == value1]['aisles'].values
     #st.write(f"Selected Aisle ID: {value1}")
 
